c9aae5407d623e27f80c1361acd72aac5760800f
vpalex999 <vpalex@mail.ru>
2022-02-13 11:50:08 +0500

    add SingIn components

---
+++ project/src/components/sing-in-message.tsx
@@ -0,0 +1,9 @@
+function SingInMessage(): JSX.Element {
+  return (
+    <div className="sign-in__message">
+      <p>We can’t recognize this email <br> and password combination. Please try again.</br></p>
+    </div>
+  );
+}
+
+export default SingInMessage;
