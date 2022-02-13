9435997b3685b5152a457fe8d42311947fd6563d
vpalex999 <vpalex@mail.ru>
2022-02-13 17:35:58 +0500

    refactoring SingInMessage

--- project/src/components/sing-in-message.tsx
+++
@@ -1,9 +0,0 @@
-function SingInMessage(): JSX.Element {
-  return (
-    <div className="sign-in__message">
-      <p>We can’t recognize this email <br> and password combination. Please try again.</br></p>
-    </div>
-  );
-}
-
-export default SingInMessage;
