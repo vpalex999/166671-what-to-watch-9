c497cef9bfec7bed18e2812803aedfda3e16e150
vpalex999 <vpalex@mail.ru>
2022-02-28 18:19:22 +0500

    add CardButtons component

--- project/src/pages/film/film.tsx
+++ project/src/pages/film/film.tsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router-dom';
+import CardButtons from '../../components/card-buttons/card-buttons';
 import UserBlock from '../../components/user-block/user-block';
+import { AppRoute } from '../../const';
 
 function Film(): JSX.Element {
+
   return (
     <div>
       <section className="film-card film-card--full">
         <div className="film-card__hero">
           <div className="film-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+            <img
+              src="img/bg-the-grand-budapest-hotel.jpg"
+              alt="The Grand Budapest Hotel"
+            />
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
@@ -29,22 +36,11 @@ function Film(): JSX.Element {
                 <span className="film-card__genre">Drama</span>
                 <span className="film-card__year">2014</span>
               </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-                <a href="add-review.html" className="btn film-card__button">Add review</a>
-              </div>
+              <CardButtons>
+                <Link to={AppRoute.AddReview} className="btn film-card__button">
+                  Add review
+                </Link>
+              </CardButtons>
             </div>
           </div>
         </div>
@@ -52,20 +48,31 @@ function Film(): JSX.Element {
         <div className="film-card__wrap film-card__translate-top">
           <div className="film-card__info">
             <div className="film-card__poster film-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+              <img
+                src="img/the-grand-budapest-hotel-poster.jpg"
+                alt="The Grand Budapest Hotel poster"
+                width="218"
+                height="327"
+              />
             </div>
 
             <div className="film-card__desc">
               <nav className="film-nav film-card__nav">
                 <ul className="film-nav__list">
                   <li className="film-nav__item film-nav__item--active">
-                    <a href="#todo" className="film-nav__link">Overview</a>
+                    <a href="#todo" className="film-nav__link">
+                      Overview
+                    </a>
                   </li>
                   <li className="film-nav__item">
-                    <a href="#todo" className="film-nav__link">Details</a>
+                    <a href="#todo" className="film-nav__link">
+                      Details
+                    </a>
                   </li>
                   <li className="film-nav__item">
-                    <a href="#todo" className="film-nav__link">Reviews</a>
+                    <a href="#todo" className="film-nav__link">
+                      Reviews
+                    </a>
                   </li>
                 </ul>
               </nav>
@@ -79,13 +86,32 @@ function Film(): JSX.Element {
               </div>
 
               <div className="film-card__text">
-                <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&apos;s friend and protege.</p>
+                <p>
+                  In the 1930s, the Grand Budapest Hotel is a popular European
+                  ski resort, presided over by concierge Gustave H. (Ralph
+                  Fiennes). Zero, a junior lobby boy, becomes Gustave&apos;s
+                  friend and protege.
+                </p>
 
-                <p>Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave&apos;s lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
+                <p>
+                  Gustave prides himself on providing first-class service to the
+                  hotel&apos;s guests, including satisfying the sexual needs of
+                  the many elderly women who stay there. When one of
+                  Gustave&apos;s lovers dies mysteriously, Gustave finds himself
+                  the recipient of a priceless painting and the chief suspect in
+                  her murder.
+                </p>
 
-                <p className="film-card__director"><strong>Director: Wes Anderson</strong></p>
+                <p className="film-card__director">
+                  <strong>Director: Wes Anderson</strong>
+                </p>
 
-                <p className="film-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+                <p className="film-card__starring">
+                  <strong>
+                    Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe
+                    and other
+                  </strong>
+                </p>
               </div>
             </div>
           </div>
@@ -99,37 +125,65 @@ function Film(): JSX.Element {
           <div className="catalog__films-list">
             <article className="small-film-card catalog__films-card">
               <div className="small-film-card__image">
-                <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+                <img
+                  src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
+                  alt="Fantastic Beasts: The Crimes of Grindelwald"
+                  width="280"
+                  height="175"
+                />
               </div>
               <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+                <a className="small-film-card__link" href="film-page.html">
+                  Fantastic Beasts: The Crimes of Grindelwald
+                </a>
               </h3>
             </article>
 
             <article className="small-film-card catalog__films-card">
               <div className="small-film-card__image">
-                <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
+                <img
+                  src="img/bohemian-rhapsody.jpg"
+                  alt="Bohemian Rhapsody"
+                  width="280"
+                  height="175"
+                />
               </div>
               <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
+                <a className="small-film-card__link" href="film-page.html">
+                  Bohemian Rhapsody
+                </a>
               </h3>
             </article>
 
             <article className="small-film-card catalog__films-card">
               <div className="small-film-card__image">
-                <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
+                <img
+                  src="img/macbeth.jpg"
+                  alt="Macbeth"
+                  width="280"
+                  height="175"
+                />
               </div>
               <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Macbeth</a>
+                <a className="small-film-card__link" href="film-page.html">
+                  Macbeth
+                </a>
               </h3>
             </article>
 
             <article className="small-film-card catalog__films-card">
               <div className="small-film-card__image">
-                <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
+                <img
+                  src="img/aviator.jpg"
+                  alt="Aviator"
+                  width="280"
+                  height="175"
+                />
               </div>
               <h3 className="small-film-card__title">
-                <a className="small-film-card__link" href="film-page.html">Aviator</a>
+                <a className="small-film-card__link" href="film-page.html">
+                  Aviator
+                </a>
               </h3>
             </article>
           </div>
--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,16 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import CardButtons from '../../components/card-buttons/card-buttons';
 import FilmList from '../../components/film-list/film-list';
 import UserBlock from '../../components/user-block/user-block';
-import { AppRoute } from '../../const';
 import { FilmData } from '../../types/film';
 
 type MainScreenProps = {
   promo: FilmData;
   films: FilmData[];
-}
+};
 
 function Main({ promo, films }: MainScreenProps): JSX.Element {
-  const navigate = useNavigate();
 
   return (
     <div>
@@ -35,7 +33,12 @@ function Main({ promo, films }: MainScreenProps): JSX.Element {
         <div className="film-card__wrap">
           <div className="film-card__info">
             <div className="film-card__poster">
-              <img src={promo.poster} alt={promo.title} width="218" height="327" />
+              <img
+                src={promo.poster}
+                alt={promo.title}
+                width="218"
+                height="327"
+              />
             </div>
 
             <div className="film-card__desc">
@@ -44,29 +47,7 @@ function Main({ promo, films }: MainScreenProps): JSX.Element {
                 <span className="film-card__genre">{promo.genre}</span>
                 <span className="film-card__year">{promo.released}</span>
               </p>
-
-              <div className="film-card__buttons">
-                <button
-                  onClick={() => { navigate(AppRoute.Play); }}
-                  className="btn btn--play film-card__button"
-                  type="button"
-                >
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button
-                  onClick={() => { navigate(AppRoute.MyList); }}
-                  className="btn btn--list film-card__button"
-                  type="button"
-                >
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-              </div>
+              <CardButtons />
             </div>
           </div>
         </div>
@@ -78,41 +59,63 @@ function Main({ promo, films }: MainScreenProps): JSX.Element {
 
           <ul className="catalog__genres-list">
             <li className="catalog__genres-item catalog__genres-item--active">
-              <a href="#todo" className="catalog__genres-link">All genres</a>
+              <a href="#todo" className="catalog__genres-link">
+                All genres
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Comedies</a>
+              <a href="#todo" className="catalog__genres-link">
+                Comedies
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Crime</a>
+              <a href="#todo" className="catalog__genres-link">
+                Crime
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Documentary</a>
+              <a href="#todo" className="catalog__genres-link">
+                Documentary
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Dramas</a>
+              <a href="#todo" className="catalog__genres-link">
+                Dramas
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Horror</a>
+              <a href="#todo" className="catalog__genres-link">
+                Horror
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Kids & Family</a>
+              <a href="#todo" className="catalog__genres-link">
+                Kids & Family
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Romance</a>
+              <a href="#todo" className="catalog__genres-link">
+                Romance
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Sci-Fi</a>
+              <a href="#todo" className="catalog__genres-link">
+                Sci-Fi
+              </a>
             </li>
             <li className="catalog__genres-item">
-              <a href="#todo" className="catalog__genres-link">Thrillers</a>
+              <a href="#todo" className="catalog__genres-link">
+                Thrillers
+              </a>
             </li>
           </ul>
 
           <FilmList films={films} />
 
           <div className="catalog__more">
-            <button className="catalog__button" type="button">Show more</button>
+            <button className="catalog__button" type="button">
+              Show more
+            </button>
           </div>
         </section>
 
