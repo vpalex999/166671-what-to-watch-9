85fe51de1f1a01896fa7b9bb781eef4c6cbe65cd
vpalex999 <vpalex@mail.ru>
2022-03-13 20:08:01 +0500

    fix component Logo

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -8,7 +8,7 @@ type LogoProps = {
 function Logo(props: LogoProps): JSX.Element {
   return (
     <div className="logo">
-      <Link to={AppRoute.Root} className={`logo__link ${props.className}`}>
+      <Link to={AppRoute.Root} className={`logo__link ${props.className || ''}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
