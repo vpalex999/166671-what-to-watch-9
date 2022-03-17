55e06cd669629c81b237a7eea229e36c90690e55
vpalex999 <vpalex@mail.ru>
2022-03-17 14:25:06 +0500

    delete fillFilmsAction

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,13 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Action } from '../const';
-import { FilmData } from '../types/film';
 
 export const changeGenreAction = createAction(
   Action.CHANGE_GENRE,
   (value: string) => ({ payload: value }),
-);
-
-export const fillFilmsAction = createAction(
-  Action.FILL_FILMS,
-  (value: FilmData[]) => ({ payload: value }),
 );
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { FilmData } from '../types/film';
-import { changeGenreAction, fillFilmsAction } from '../store/action';
+import { changeGenreAction } from '../store/action';
 import { ALL_GENRES } from '../const';
 import { mokFilmList } from '../moks/films';
 
@@ -15,13 +15,9 @@ const initialState: InitialState = {
 };
 
 const reducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(changeGenreAction, (state, action) => {
-      state.filterGenre = action.payload;
-    })
-    .addCase(fillFilmsAction, (state, action) => {
-      state.films = action.payload;
-    });
+  builder.addCase(changeGenreAction, (state, action) => {
+    state.filterGenre = action.payload;
+  });
 });
 
 export { reducer };
