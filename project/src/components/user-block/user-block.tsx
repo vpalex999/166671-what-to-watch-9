cd48f54c3d8ef3263076667b710751a6427ffbff
vpalex999 <vpalex@mail.ru>
2022-04-08 16:04:17 +0500

    start refactoring user-block

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -7,38 +7,45 @@ import { setAuthorizationAction } from '../../store/user-process/user-process';
 
 function UserBlock(): JSX.Element {
   const navigate = useNavigate();
-  const { authorizationStatus } = useAppSelector(({USER}) => USER);
+  const { authorizationStatus } = useAppSelector(({ USER }) => USER);
   const dispatch = useDispatch();
   const isAuthorized = authorizationStatus === AuthorizationStatus.Auth;
 
-  return (
-    <ul className="user-block">
-      <li className="user-block__item">
-        <div
-          className="user-block__avatar"
-          onClick={() => {
-            navigate(AppRoute.MyList);
-          }}
-        >
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-        </div>
-      </li>
-      <li className="user-block__item">
-        <Link
-          to={isAuthorized ? AppRoute.Root : AppRoute.Login}
-          className="user-block__link"
-          onClick={(evt) => {
-            if (isAuthorized) {
+  if (isAuthorized) {
+
+    return (
+      <ul className="user-block">
+        <li className="user-block__item">
+          <div
+            className="user-block__avatar"
+            onClick={() => {
+              navigate(AppRoute.MyList);
+            }}
+          >
+            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          </div>
+        </li>
+        <li className="user-block__item">
+          <Link
+            to={AppRoute.Root}
+            className="user-block__link"
+            onClick={(evt) => {
               evt.preventDefault();
               dispatch(setAuthorizationAction(AuthorizationStatus.NoAuth));
               dispatch(logoutAction());
-            }
-          }}
-        >
-          {isAuthorized ? 'Sign out' : 'Sign In'}
-        </Link>
-      </li>
-    </ul>
+            }}
+          >
+            Sign out
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
+  return (
+    <div className="user-block">
+      <Link to={AppRoute.Login} className="user-block__link">Sign in</Link>
+    </div>
   );
 }
 
